206db195475af75dd6bce377d3133478fa90e489
Paul2803k <p.scattolin95@gmail.com>
2019-04-10 02:00:35 +0200

    trying to add a reduce function that sums all the similar terms in the poly. Still doesn't work

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Polynomial p1 = new Polynomial( "3.0 1 2 3" );
         Polynomial p2 = new Polynomial( "4 0 5 3" );
-        Polynomial p3 = new Polynomial( "4 0 -5 3" );
+        Polynomial p3 = new Polynomial( "3 1 -5 3" );
         Polynomial p4 = new Polynomial( "6 1 4 4 4 0" );
         
         System.out.println( p1);
@@ -19,9 +19,10 @@ public class Main {
         System.out.println( p3 );
         System.out.println( p4 );
         
-        p1.minus(p3);
-        p1.evaluate(2);
+        p1.times(p1);
         System.out.println( p1);
+        p1.evaluate(2);
+        
         
    }
 
