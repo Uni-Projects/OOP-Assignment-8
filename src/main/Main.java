3ac767a0e1bbb3ba4e482e83b64d3d023306f2e6
Paul2803k <p.scattolin95@gmail.com>
2019-04-12 09:02:51 +0200

    fix equals

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,21 +9,22 @@ import polynomial.Polynomial;
 public class Main {
 
     public static void main(String[] args) {
-        Polynomial p1 = new Polynomial( "3.0 1 2 3" );
-        Polynomial p2 = new Polynomial( "4 0 5 3" );
-        Polynomial p3 = new Polynomial( "3 1 -5 3" );
-        Polynomial p4 = new Polynomial( "6 1 4 4 4 0" );
+        Polynomial p1 = new Polynomial("3.0 1 2 3");
+        Polynomial p2 = new Polynomial("4 0 5 3");
+        Polynomial p3 = new Polynomial("3 1 -5 3");
+        Polynomial p4 = new Polynomial("6 1 4 4 4 0");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+        if(p1.equals(p2))
+            System.out.println("equals");
         
-        System.out.println( p1);
-        System.out.println( p2 );
-        System.out.println( p3 );
-        System.out.println( p4 );
-        
-        p1.times(p1);
-        System.out.println( p1);
+        p1.times(p3);
+        System.out.println(p1);
         p1.evaluate(2);
-        
-        
-   }
+
+    }
 
 }
