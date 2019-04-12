5ccb0ce4a501f45733fbee25e52a6293f8400328
Paul2803k <p.scattolin95@gmail.com>
2019-04-12 10:22:54 +0200

    fix times & finishing touches, still no Java Test tho

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,18 +12,15 @@ public class Main {
         Polynomial p1 = new Polynomial("3.0 1 2 3");
         Polynomial p2 = new Polynomial("4 0 5 3");
         Polynomial p3 = new Polynomial("3 1 -5 3");
-        Polynomial p4 = new Polynomial("6 1 4 4 4 0");
+        Polynomial p4 = new Polynomial("-3 1 4 4 4 0");
 
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
         System.out.println(p4);
-        if(p1.equals(p2))
-            System.out.println("equals");
-        
-        p1.times(p3);
+        p1.minus(p2);
         System.out.println(p1);
-        p1.evaluate(2);
+        p1.evaluate(0);
 
     }
 
